a168ce47bff921bc0f706f5091e78529f86c9921
Mo59471 <9674025@graniteschools.org>
2026-02-05 12:07:16 -0700

    Add files via upload

---
+++ src/MadLibs/MadLibs.py
@@ -0,0 +1,30 @@
+replay = True
+while replay == True:
+    print("Welcome to Murder Mystery Mad Libs! Please choose one of the following stories to complete: ")
+    print("1. Story 1")
+    print("2. Story 2")
+    print("3. Story 3")
+    print("4. Story 4")
+    while True:
+        try:
+            storyNum = int(input("Enter your selection (story number) here: "))
+        except ValueError:
+            print("Please input a valid story selection.")
+        else:
+            if storyNum > 4 or storyNum < 1:
+                print("Please input a valid story selection.")
+            else:
+                break
+    if storyNum == 1:
+        pass
+    elif storyNum == 2:
+        pass
+    elif storyNum == 3:
+        pass
+    else:
+        pass
+    if input("Would you like to make another story? Enter 'y' to continue: ") != 'y':
+        replay == False
+        print("Awww... you could have had so much fun. Your life must be so sad and depressing. Bye Bye.")
+    
+
