def9299314670385fe09cb46286ffa41b6af5c2c
9674036-code <9674036@graniteschools.org>
2026-02-06 13:11:23 -0700

    Update MadLibsStory.py

--- src/MadLibs/MadLibsStory.py
+++ src/MadLibs/MadLibsStory.py
@@ -8,9 +8,9 @@ class MadLibsStory:
 
         #All four stories are stored in a string list, and referred to using their indexes
         story = [
-"""I want to solve a murder mystery. Why I wish to solve one is something I don’t know. I simply want to, almost as if this desire was the result of a mood. The murder-mystery-mood, I suppose. The sky today was quite clear and blue — Oh, I suppose that this must bore the reader. Topics regarding the weather are generally accepted as a sign of idle boredom, after all.
+"""I want to solve a murder mystery. Why I wish to solve one is something I don’t know. I simply want to, almost as if this desire was the result of a mood. The murder-mystery-mood, I suppose. The sky today was quite clear and blue — Oh, I suppose that this must bore the reader. Topics regarding the weather are generally accepted as a sign of idle boredom, after all. Very well, let's try something innovative instead: the concrete of the sidewalk firmly pressed into my feet; today’s ground was much more rigid than usual. Perhaps it was reassuring to know that the ground was a firm support always at arm’s, or rather leg’s length; to know that the ground will be forever-
 
-@sound*.
+@expression of suprise*
 
 Huh. This must be what they call @saying*. Wait, what is this nonsense? Apologies, it seems that I stuttered; I meant “The art of being wise is the art of knowing what to overlook.” What does that mean? I have no idea, it seems that my author was unable to find a suitable, well established saying to express my sentiment, after all, if anything, I am not wise. Either way, to think that I would find something to satiate my murder-mystery-mood so quickly; it makes me wonder: am I in this situation because I am lucky or because I was actively looking for it?
 
@@ -22,7 +22,9 @@ Epilogue:
 
 Now for the punchline, or rather, the slap to my face. After standing for a while, I decided to inspect the flowers more carefully. I walked to the other side of the road, after checking both directions, obviously (I did not want to end up like the person the flowers were offered to). I first decided to touch the flowers, and upon doing so, they wilted, for they had begun melting in my hands. How could flowers melt? It seems that the flowers were not flowers, but flower-shaped chocolate made by the people on the internet that make realistic objects with chocolate. The entire time, the “flowers” were not there to honor a deceased person but to warn against crossing the road, and I, the fool, had fallen for the hook, line and sinker and then reeled myself in. And to think that I had been denied the simple dignity of being deceived by real, genuine flowers and had instead been bamboozled by chocolate! It truly was as if someone slapped my face and then decided to stomp on it. I slowly stood up and walked away, as if I was running away from an unpleasant experience not metaphorically, but literally, yet I doubted that distance could erase my memories of this.
 
-"Nonsense."""]  
+"Nonsense."
+"""
+]  
         
         #The '*' and '@' mark the placeholder words in the mad lib to be cut and substituted with user inputs
         while "*" in story[self.sNum-1]:
