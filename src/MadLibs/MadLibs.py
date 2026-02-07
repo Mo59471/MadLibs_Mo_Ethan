762fbf3f2b7bbc02465abf14357577a1ebe5d04d
9674036-code <9674036@graniteschools.org>
2026-02-06 19:33:44 -0700

    Update MadLibs.py

--- src/MadLibs/MadLibs.py
+++ src/MadLibs/MadLibs.py
@@ -3,10 +3,10 @@ from MadLibsStory import MadLibsStory
 replay = True
 while replay == True:
     print("Welcome to Murder Mystery Mad Libs! Please choose one of the following stories to complete: ")
-    print("1. Story 1")
-    print("2. Story 2")
-    print("3. Story 3")
-    print("4. Story 4")
+    print("1. I Want to Solve a Murder Mystery")
+    print("2. Humanity's greatest serial killer")
+    print("3. Murder Uno Reverse")
+    print("4. The Mahjong Murder")
     while True:
         try:
             storyNum = int(input("Enter your selection (story number) here: "))
